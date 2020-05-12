99b4964a9e8240add3a27c8524858b4d5b43cbee
jijo0465 <jijo0465@gmail.com>
2020-05-13 01:27:10 +0530

    First Commit

---
+++ src/main/java/com/monkmind/digimobileapi/DigimobileapiApplication.java
@@ -0,0 +1,13 @@
+package com.monkmind.digimobileapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigimobileapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigimobileapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/monkmind/digimobileapi/DigimobileapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monkmind.digimobileapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigimobileapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
